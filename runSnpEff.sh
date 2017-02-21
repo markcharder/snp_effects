e6314af6da462962c75644f1da17cfabfdeecab8
mark <markcharder@gmail.com>
2017-02-21 13:11:21 +0800

    Fixed run file.

--- runSnpEff.sh
+++ runSnpEff.sh
@@ -7,6 +7,11 @@
 EFFPATH=/home/mark/local/src/snpEff #customise
 INPUTS=($(ls /home/mark/Rdrive/CCDM_Prog_10_Share-HANE0J-SE00128/Sclerotinia/pan_genome/outputs/Pawsey_02-17/*.var/*.highqual.vcf)) #customise
 INTERPROPATH=/home/mark/Analyses/2017/02.17/filter_genes/interpro.filtered.txt #customise
+OUTDIR=$1
+
+if [ ! -d $1 ]; then 
+	mkdir $1
+fi
 
 # Function to get commands for running snpEff in parallel on an array of inputs from the inputs directory.
 function get_commands()
@@ -20,7 +25,7 @@ function get_commands()
 		stub=${array[$i]}
 		stub=${stub##*/}
 		stub=${stub%.*}
-		effcommands+=("java -jar $EFFPATH/snpEff.jar eff Ssclerotiorum_v2 -stats $stub ${array[$i]} > $stub.vcf")
+		effcommands+=("java -jar $EFFPATH/snpEff.jar eff Ssclerotiorum_v2 -stats $OUTDIR/$stub ${array[$i]} > $OUTDIR/$stub.vcf")
 	done
 
 	tabcommands=()
@@ -29,7 +34,7 @@ function get_commands()
 		stub=${array[$i]}
 		stub=${stub##*/}
 		stub=${stub%.*}
-		tabcommands+=("perl onePerLine.pl $stub.vcf > $stub.opl.vcf")
+		tabcommands+=("perl onePerLine.pl $OUTDIR/$stub.vcf > $OUTDIR/$stub.opl.vcf")
 	done
 
 }
@@ -50,8 +55,8 @@ function interpro_formatting()
 		newarray+=("$name	")
 	done
 
-	echo ${newarray[*]} | tr ' ' '\n' |  fgrep -f - $INTERPROPATH > $stub.interpro.txt
-	perl InterProScanToGOstats.pl -i $stub.interpro.txt -p $stub.gos
+	echo ${newarray[*]} | tr ' ' '\n' |  fgrep -f - $INTERPROPATH > $OUTDIR/$stub.interpro.txt
+	perl InterProScanToGOstats.pl -i $OUTDIR/$stub.interpro.txt -p $OUTDIR/$stub.gos
 
 }
 
@@ -59,30 +64,30 @@ function interpro_formatting()
 get_commands
 
 # Check if script has been run in current directory, if not do the appropriate process. First snpEff, then reformatting commands.
-if ! ls *vcf > /dev/null 2>&1; then
+if ! ls $OUTDIR/*vcf > /dev/null 2>&1; then
 	parallel --no-notice --jobs 6 ::: "${effcommands[@]}"
 fi
 
-if ! ls *opl.vcf > /dev/null 2>&1; then
+if ! ls $OUTDIR/*opl.vcf > /dev/null 2>&1; then
 	parallel --no-notice --jobs 6 ::: "${tabcommands[@]}"
 fi
 
-if ! ls *mf.txt > /dev/null 2>&1; then
+if ! ls $OUTDIR/*mf.txt > /dev/null 2>&1; then
 	for file in ${INPUTS[@]}; do
 		stub=$file
 		stub=${stub##*/}
 		stub=${stub%.*}
-		grep "HIGH"	$stub.opl.vcf | awk 'BEGIN{OFS="\t"}{split($0,a,"|");print a[2],a[3],a[4]}' > $stub.HIGH.txt
-		grep "MODERATE"	$stub.opl.vcf | awk 'BEGIN{OFS="\t"}{split($0,a,"|");print a[2],a[3],a[4]}' > $stub.MODERATE.txt
-		grep "LOW"	$stub.opl.vcf | awk 'BEGIN{OFS="\t"}{split($0,a,"|");print a[2],a[3],a[4]}' > $stub.LOW.txt
+		grep "HIGH"	$OUTDIR/$stub.opl.vcf | awk 'BEGIN{OFS="\t"}{split($0,a,"|");print a[2],a[3],a[4]}' > $OUTDIR/$stub.HIGH.txt
+		grep "MODERATE"	$OUTDIR/$stub.opl.vcf | awk 'BEGIN{OFS="\t"}{split($0,a,"|");print a[2],a[3],a[4]}' > $OUTDIR/$stub.MODERATE.txt
+		grep "LOW"	$OUTDIR/$stub.opl.vcf | awk 'BEGIN{OFS="\t"}{split($0,a,"|");print a[2],a[3],a[4]}' > $OUTDIR/$stub.LOW.txt
 	done
 fi
 
 # Now get interpro domains using previous function and reformat for GOstats.
 if ! ls *interpro.txt >/dev/null 2>&1; then
 
-	lowarray=($(ls *LOW.txt))
-	modarray=($(ls *MODERATE.txt))
+	lowarray=($(ls $OUTDIR/*LOW.txt))
+	modarray=($(ls $OUTDIR/*MODERATE.txt))
 
 	for file in ${lowarray[@]}; do
 		interpro_formatting $file
@@ -91,15 +96,16 @@ if ! ls *interpro.txt >/dev/null 2>&1; then
 		stub=$file
 		stub=${stub##*/}
 		stub=${stub%.*}
-		array=($(awk '[print $3}' $file))
+		stub=${stub%/MODERATE/HIGH}
+		array=($(awk '{print $3}' $file))
 		for name in ${array[@]}; do
 			newarray+=("$name\n")
 		done
-		echo ${newarray[*]} | tr ' ' '\n' | fgrep -f - $stub.HIGH.txt > $stub.temp
-		mv $stub.temp $stub.HIGH.txt
+		echo ${newarray[*]} | tr ' ' '\n' | fgrep -v -f - $OUTDIR/$stub.txt > $OUTDIR/$stub.temp
+		mv $OUTDIR/$stub.temp $OUTDIR/$stub.txt
 		interpro_formatting $file
 	done
-	higharray=($(ls *HIGH.txt))
+	higharray=($(ls $OUTDIR/*HIGH.txt))
 	for file in ${higharray[@]}; do
 		interpro_formatting $file
 	done
